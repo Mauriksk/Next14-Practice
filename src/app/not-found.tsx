3cf68da9a39f1e383cb2222b658dfa62aa72016e
Mauriksk <otarksk@gmail.com>
2024-06-21 21:39:08 -0300

    Fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,6 +11,7 @@ export default function NotFound() {
           <h1 className="text-9xl font-extrabold text-white tracking-widest">
             404
           </h1>
+          
           <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
             Pokémon no encontrado
           </div>
